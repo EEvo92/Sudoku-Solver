1a464bd9254f837ee3efdc7aee77da20d4a971eb
EEvo92 <fonsecalopezjose@gmail.com>
2022-12-11 00:52:09 +0100

    Point of view changed. Algorithm improved. New Look and Feel, or something.

--- Sudoku Solver/Models/Casilla.cs
+++ Sudoku Solver/Models/Casilla.cs
@@ -1,29 +1,21 @@
-﻿using System.Linq;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Sudoku_Solver.Models
 {
-    class Casilla
+    public class Casilla
     {
-        public bool solucionado;
-
         public int fila;
 
         public int columna;
 
         public int supercelda;
         public int Numero { get; set; }
-        //public int[] Posibles { get; set; }
-
+        
         public List<int> Posibles { get; set; }
 
         public Casilla(int num, int fil, int col, int sup)
         {
-            if (num != 0) { solucionado = true;}
-            else { solucionado = false; }
-
             Numero = num;
-            //Posibles = new int []{ 1, 2, 3, 4, 5, 6, 7, 8, 9};
             Posibles = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             fila = fil;
@@ -36,7 +28,6 @@ namespace Sudoku_Solver.Models
             this.fila = cas.fila;
             this.columna = cas.columna;
             this.supercelda = cas.supercelda;
-            this.solucionado = cas.solucionado;
             this.Numero = cas.Numero;           
             this.Posibles = cas.Posibles;
         }
